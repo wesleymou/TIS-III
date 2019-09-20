70b6e198dd59e291bd34efd4789247a3d05848fb
Rafael Botazini <rafaelbotazini@gmail.com>
2019-09-20 02:22:32 -0300

    View cliente: cadastrar, visualizar e pesquisar

--- src/routes/customer.ts
+++ src/routes/customer.ts
@@ -2,39 +2,41 @@ import createError from 'http-errors';
 import { Router, Request, Response, NextFunction } from "express";
 import CustomerService from "../services/CustomerService";
 import Customer from '../models/Customer';
+import CustomerListViewModel from '../models/CustomerListViewModel';
 
 const service = new CustomerService();
 const router = Router();
 
 router.get('/', async (req, res, next) => {
-    try {
-        const query: string | undefined = req.query.q;
+  try {
+    const query: string | undefined = req.query.q;
 
-        const customers = query
-            ? await service.searchAsync(query)
-            : await service.getAllAsync();
+    const customers = query
+      ? await service.searchAsync(query)
+      : await service.getAllAsync();
 
-        res.json(customers);
+    const viewModel = new CustomerListViewModel(customers);
 
-    } catch (err) {
-        next(createError(500, err));
-    }
+    res.render('customer-list', { title: 'Figaro - Clientes', ...viewModel });
+  } catch (err) {
+    next(createError(500, err));
+  }
 });
 
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const customer = validateCustomerFromRequestBody(req.body);
+  try {
+    const customer = validateCustomerFromRequestBody(req.body);
 
-        if (customer == null) {
-            return next(createError(400, new Error('Informações inválidas.')))
-        }
-
-        await service.createAsync(customer);
-
-        res.status(201).send();
-    } catch (err) {
-        next(createError(500, err));
+    if (customer == null) {
+      return next(createError(400, new Error('Informações inválidas.')))
     }
+
+    await service.createAsync(customer);
+
+    res.status(201).send();
+  } catch (err) {
+    next(createError(500, err));
+  }
 });
 
 /**
@@ -43,31 +45,30 @@ router.post('/', async (req: Request, res: Response, next: NextFunction) => {
  * @param obj Objeto que será convertido
  */
 function validateCustomerFromRequestBody(obj: any): Customer | null {
-    const {
-        fullName,
-        nickname,
-        phone,
-        email,
-        address
-    } = obj;
+  const {
+    fullName,
+    nickname,
+    phone,
+    email,
+    address
+  } = obj;
 
-    if (!fullName || !phone) {
-        return null;
-    }
+  if (!fullName || !phone) {
+    return null;
+  }
 
-    const customer = new Customer();
+  const customer = new Customer();
 
-    const result: Customer = {
-        ...customer,
-        fullName,
-        nickname,
-        phone,
-        email,
-        address
-    };
+  const result: Customer = {
+    ...customer,
+    fullName,
+    nickname,
+    phone,
+    email,
+    address
+  };
 
-    return result;
+  return result;
 }
-
 
 export default router;
