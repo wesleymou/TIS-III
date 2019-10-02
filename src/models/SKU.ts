a82f973c3287d68ee3eea1c64ddaa04b8298951a
wesleymou <wesley.pra@hotmail.com>
2019-10-01 21:09:16 -0300

    model SKU

---
+++ src/models/SKU.ts
@@ -0,0 +1,11 @@
+class SKU {
+    productId: number = 0;
+    quantityAvailable: number = 0;
+    price: number = 0;
+    dateCreated?: Date;
+    expirationDate?: Date;
+    
+    
+}
+
+export default SKU;
