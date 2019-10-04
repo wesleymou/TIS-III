b7d4dd170a4a431282de03681f9e39f4e5b56261
Rafael Botazini <rafaelbotazini@gmail.com>
2019-10-04 11:21:03 -0300

    Create SKU table

--- scripts/create-tables.sql
+++ scripts/create-tables.sql
@@ -11,6 +11,21 @@ CREATE TABLE product (
 	CONSTRAINT product_PK PRIMARY KEY (id)
 );
 
+-- Cria tabela de unidade de estoque
+CREATE TABLE sku (
+	id int NULL AUTO_INCREMENT,
+	product_id INT NOT NULL,
+	name varchar(256) NULL,
+	description varchar(512) NOT NULL,
+	date_expires DATETIME NULL,
+	date_created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	quantity_purchased int NOT NULL,
+	quantity_available int NOT NULL,
+	price decimal(13,2) NOT NULL,
+	CONSTRAINT sku_PK PRIMARY KEY (id),
+	CONSTRAINT sku_product_FK FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
 -- Cria tabela de clientes
 CREATE TABLE customer (
 	id INT NOT NULL AUTO_INCREMENT,
