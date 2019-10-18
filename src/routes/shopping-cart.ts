2af990a03d905c6be58c95ef9f183f682b993618
wesleymou <wesley.pra@hotmail.com>
2019-10-18 19:25:40 -0300

    fix: mostrar produtos na tela de registrar vendas

--- src/routes/shopping-cart.ts
+++ src/routes/shopping-cart.ts
@@ -1,16 +1,16 @@
-import { isArray } from 'util';
+import { isArray } from "util";
 import { Router } from "express";
-import createError from 'http-errors';
+import createError from "http-errors";
 
-import SaleService from '../services/SaleService';
-import SKUService from '../services/SKUService';
+import SaleService from "../services/SaleService";
+import SKUService from "../services/SKUService";
 
-import ProductListViewModel from '../models/ProductListViewModel';
-import SKU from '../models/SKU';
-import Sale from '../models/Sale';
-import SaleItem from '../models/SaleItem';
+import ProductListViewModel from "../models/ProductListViewModel";
+import SKU from "../models/SKU";
+import Sale from "../models/Sale";
+import SaleItem from "../models/SaleItem";
 
-import { checkAuthToken } from '../middlewares/session-check';
+import { checkAuthToken } from "../middlewares/session-check";
 
 const skuService = new SKUService();
 const saleService = new SaleService();
@@ -19,11 +19,16 @@ const router = Router();
 
 router.use(checkAuthToken);
 
-router.get('/', async (req, res) => {
-  res.render('shopping-cart', { title: 'Figaro - Registrar venda' });
+router.get("/", async (req, res) => {
+  const skuList = await skuService.getAllAsync();
+  const viewModel = new ProductListViewModel(skuList);
+  res.render("shopping-cart", {
+    title: "Figaro - Estoque",
+    product: viewModel.products
+  });
 });
 
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
     let sale;
 
@@ -34,16 +39,18 @@ router.post('/', async (req, res, next) => {
     }
 
     await saleService.createAsync(sale);
-    res.status(201).send('OK');
+    res.status(201).send("OK");
   } catch (err) {
     return next(createError(500, err));
   }
 });
 
-router.get('/:query', async (req, res, next) => {
+router.get("/:query", async (req, res, next) => {
   try {
     const { query } = req.params;
-    let products: SKU[] = query ? await skuService.searchProductAsync(query) : [];
+    let products: SKU[] = query
+      ? await skuService.searchProductAsync(query)
+      : [];
     const viewModel = new ProductListViewModel(products);
     res.json(viewModel);
   } catch (err) {
@@ -52,10 +59,7 @@ router.get('/:query', async (req, res, next) => {
 });
 
 async function validateSale(obj: any): Promise<Sale> {
-  const {
-    items,
-    discount
-  } = obj;
+  const { items, discount } = obj;
 
   const normalDiscount = Math.abs(Number(discount));
 
@@ -63,43 +67,53 @@ async function validateSale(obj: any): Promise<Sale> {
     !isArray(items) ||
     !items.length ||
     isNaN(normalDiscount) ||
-    normalDiscount > 100) {
-    throw new Error('Informações inválidas!');
+    normalDiscount > 100
+  ) {
+    throw new Error("Informações inválidas!");
   }
 
-  if (items.some((item: SaleItem) => !Number(item.skuId) || !Number(item.quantity))) {
-    throw new Error('Itens inválidos!');
+  if (
+    items.some(
+      (item: SaleItem) => !Number(item.skuId) || !Number(item.quantity)
+    )
+  ) {
+    throw new Error("Itens inválidos!");
   }
 
   const skus = await skuService.searchByIdAsync(items.map(i => i.skuId));
 
   if (skus.length !== items.length) {
-    throw new Error('Itens não encontrados!');
+    throw new Error("Itens não encontrados!");
   }
 
-  const quantityUnavailable = skus.some((sku) => {
+  const quantityUnavailable = skus.some(sku => {
     const item = items.find(i => i.skuId === sku.id);
     return sku.quantityAvailable < item.quantity;
   });
 
   if (quantityUnavailable) {
-    throw new Error('Produto fora de estoque!');
+    throw new Error("Produto fora de estoque!");
   }
 
   const sale = new Sale();
 
   sale.discount = normalDiscount / 100;
-  sale.items = skus.map((sku) => Object.assign(new SaleItem(), {
-    skuId: sku.id,
-    name: sku.name,
-    description: sku.description,
-    expirationDate: sku.expirationDate,
-    price: sku.price,
-    quantity: items.find(i => i.skuId === sku.id).quantity,
-    priceSold: sku.price * (1 - sale.discount),
-  }));
+  sale.items = skus.map(sku =>
+    Object.assign(new SaleItem(), {
+      skuId: sku.id,
+      name: sku.name,
+      description: sku.description,
+      expirationDate: sku.expirationDate,
+      price: sku.price,
+      quantity: items.find(i => i.skuId === sku.id).quantity,
+      priceSold: sku.price * (1 - sale.discount)
+    })
+  );
 
-  sale.totalPrice = sale.items.reduce((total, item) => (item.priceSold * item.quantity) + total, 0);
+  sale.totalPrice = sale.items.reduce(
+    (total, item) => item.priceSold * item.quantity + total,
+    0
+  );
 
   return sale;
 }
