45966597a28069a65cd64667e8c119c8a67813c4
Rafael Botazini <rafaelbotazini@gmail.com>
2019-11-21 19:38:01 -0300

    Editar SKU

--- src/public/js/sku.js
+++ src/public/js/sku.js
@@ -1,10 +1,25 @@
+/**
+ * Id do produto atual
+ * @type {number}
+ */
+var productId;
+
 $(function () {
-  var productId = $('#productId').val();
+  // pegando o id do produto do campo oculto
+  productId = $('#productId').val();
+
+  /**
+   * Modal de adicionar/editar sku
+   */
+  const $modal = $('#modal-sku');
 
   /**
    * Itens pesquisados na query string
    */
   var searchParams = new URLSearchParams(window.location.search);
+
+
+  $('#tabelaProdutos').DataTable();
 
   // Foca o usuário no input de pesquisa ao iniciar a tela
   $('#query-input').focus();
@@ -26,34 +41,43 @@ $(function () {
     }
   });
 
+  // Abre o modal ao clicar no botão de adicionar estoque
+  $('.btn-add-sku').click(function () {
+    $modal.find('h2').html('Adicionar unidade de estoque.');
+    $modal.modal('show');
+  });
+
+  $('.edit-action').click(function () {
+    $modal.find('h2').html('Adicionar unidade de estoque.');
+
+    const id = $(this).data('id');
+
+    $.get('/product/sku/' + id)
+      .then((sku) => {
+        setModalInputs(sku);
+        $modal.modal('show');
+      })
+      .catch(() => alert('Ocorreu um erro ao buscar os dados da unidade.'));
+  });
+
   $('.remove-action').click(function () {
     const id = $(this).data('id');
 
     const ok = confirm('Remover unidade?');
 
     if (ok) {
-      $.ajax({
-        url: '/product/sku/' + id,
-        method: 'DELETE'
-      })
-        .then(function () {
-          alert('Unidade removida com successo.');
-          window.location.assign(window.location.href);
-        })
-        .catch(function (err) {
-          alert('Ocorreu um erro :(');
-          console.error(err);
-        });
+      deleteSku(id);
     }
   });
 
   // Adiciona a unidade ao clicar no botão de adicionar
-  $('#btn-create').click(function () {
+  $('#btn-save').click(function () {
     var dateVal = $('#expirationDateFormInput').val();
     var expDate = null;
 
     if (dateVal) {
-      expDate = new Date(dateVal).toISOString();
+      const [year, month, day] = dateVal.split('-').map(Number);
+      expDate = new Date(year, month - 1, day).toISOString();
     }
 
     var sku = {
@@ -62,13 +86,91 @@ $(function () {
       expirationDate: expDate,
     };
 
-    $.post('/product/' + productId, sku)
-      .then(function (response) {
-        alert('Item cadastrado com sucesso!');
-        window.location.assign('/product/view/' + productId);
-      })
-      .fail(function () {
-        alert('Ocorreu um erro, verifique as informações e tente novamente.');
-      });
+    const id = $('#idFormInput').val();
+
+    if (id) {
+      editSku(id, sku);
+    } else {
+      createSku(sku);
+    }
+  });
+
+  // Limpa campos ao fechar o modal
+  $modal.on('hide.bs.modal', function () {
+    $(this).find('input').val('');
   });
 });
+
+/**
+ * Cria uma nova unidade para o produto atual
+ * @param {any} sku Informações da unidade de estoque
+ */
+function createSku(sku) {
+  $.post('/product/' + productId, sku)
+    .then(function (response) {
+      alert('Item cadastrado com sucesso!');
+      window.location.assign('/product/view/' + productId);
+    })
+    .fail(function () {
+      alert('Ocorreu um erro, verifique as informações e tente novamente.');
+    });
+}
+
+/**
+ * Atualiza os campos de um dado sku
+ * @param {number} id Id do sku
+ * @param {any} update Campos para atualizar
+ */
+function editSku(id, update) {
+  $.ajax({
+    url: '/product/sku/' + id,
+    method: 'PUT',
+    data: JSON.stringify(update),
+    contentType: 'application/json'
+  })
+    .then(function () {
+      alert('Unidade atualizada com sucesso!');
+      window.location.assign('/product/view/' + productId);
+    })
+    .fail(function () {
+      alert('Ocorreu um erro, verifique as informações e tente novamente.');
+    });
+}
+
+/**
+ * Apaga um sku do banco de dados
+ * @param {number} id Id do sku a ser removido
+ */
+function deleteSku(id) {
+  $.ajax({
+    url: '/product/sku/' + id,
+    method: 'DELETE'
+  })
+    .then(function () {
+      alert('Unidade removida com successo.');
+      window.location.assign('/product/view/' + productId);
+    })
+    .catch(function (err) {
+      alert('Ocorreu um erro :(');
+      console.error(err);
+    });
+}
+
+function setModalInputs(sku) {
+  const expDate = new Date(sku.expirationDate);
+
+  let expDateVal = '';
+
+  if (expDate.valueOf()) {
+    const day = String(expDate.getDate()).padStart(2, '0');
+    const month = String(expDate.getMonth() + 1).padStart(2, '0');
+    const year = expDate.getFullYear();
+
+    expDateVal = [year, month, day].join('-');
+  }
+
+  $('#idFormInput').val(sku.id);
+  $('#quantityFormInput').val(sku.quantityAvailable);
+  $('#priceFormInput').val(sku.price);
+  $('#expirationDateFormInput').val(expDateVal);
+}
